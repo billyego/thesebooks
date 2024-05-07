3eec75075aba2f9b1a66f8025b211d99aff17042
matthewmwachi <mattmuhande@gmail.com>
2024-05-07 10:21:12 +0300

    adding the card component's content

--- src/BookCard.js
+++ src/BookCard.js
@@ -4,7 +4,13 @@ function BookCard({books}){
     return (
         <div className = "book">
 
-
+            <h2>{books.title}</h2>
+            <img src={books.thumbnailUrl} alt={books.title} />
+            <p>{books.authors}</p>
+            <p>{books.shortDescription}</p>
+            <p>{books.pageCount}</p>
+            <p>{books.categories}</p>
+            <p>{books.publishedDate}</p>
         </div>
     )
 
