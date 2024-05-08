2caee41b5efaf61657e07830fab0cae2b970b85f
Bill <bill.kimtai@student.moringaschool.com>
2024-05-08 12:02:00 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import CollectionOfCards from "./CollectionOfCards";
@@ -8,15 +9,16 @@ import NavBar from "./NavBar";
 function App() {
   return (
     <div className="App">
-    <NavBar />
+      <NavBar />
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/books" element={<CollectionOfCards />} />
-      <Route path="/books/:id" element={<BookDetails />} />
-      <Route path="/newbook" element={<NewBook />} />
-    </Routes>
-  </div>
-);
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<CollectionOfCards />} />
+        <Route path="/book/:id" element={<BookDetails />} />
+        <Route path="/newbook" element={<NewBook />} />
+      </Routes>
+    </div>
+  );
 }
+
 export default App;
